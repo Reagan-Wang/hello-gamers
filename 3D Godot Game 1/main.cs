d281e54d99acd941752a7894c777e067dbae82df
Reagan Guan <reaganwa@usc.edu>
2023-10-29 21:44:54 -0700

    3D Godot Game Rough Commit

---
+++ 3D Godot Game 1/main.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public partial class main : Node
+{
+	[Export]
+	public PackedScene MobScene { get; set; }
+
+	private void _on_mob_timer_timeout()
+	{
+		// Create a new instance of the Mob scene.
+		Mob mob = MobScene.Instantiate<Mob>();
+
+		// Choose a random location on the SpawnPath.
+		// We store the reference to the SpawnLocation node.
+		var mobSpawnLocation = GetNode<PathFollow3D>("SpawnPath/SpawnLocation");
+		// And give it a random offset.
+		mobSpawnLocation.ProgressRatio = GD.Randf();
+
+		Vector3 playerPosition = GetNode<Player>("Player").Position;
+		mob.Initialize(mobSpawnLocation.Position, playerPosition);
+
+		// Spawn the mob by adding it to the Main scene.
+		AddChild(mob);
+	}
+
+	private void _on_player_hit()
+	{
+		GetNode<Timer>("MobTimer").Stop();
+	}
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
